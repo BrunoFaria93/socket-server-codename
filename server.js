a8a9f887bcd442efa7b64904e7c8e3e78c2458fa
Bruno Faria <soares_de_faria@hotmail.com>
2025-08-29 16:13:50 -0300

    fix

--- server.js
+++ server.js
@@ -32,7 +32,6 @@ const colors = ["#60a5fa", "#f87171", "#d1d5db", "#000000"]; // Cores diferentes
 io.on("connection", (socket) => {
   console.log(`Client connected: ${socket.id}`);
 
-  // Envia a lista de salas disponíveis ao conectar
   socket.emit(
     "rooms-update",
     Object.keys(rooms).map((roomId) => ({ roomId }))
@@ -50,14 +49,13 @@ io.on("connection", (socket) => {
         spymasters: { blue: null, red: null },
         gameStatus: "playing",
         blackWordRevealed: false,
-        currentTeam: "blue",
+        currentTeam: "red",
       };
 
       io.emit(
         "rooms-update",
         Object.keys(rooms).map((id) => ({ roomId: id }))
       );
-      console.log(`Room created: ${roomId}`);
     }
 
     socket.join(roomId);
@@ -66,8 +64,6 @@ io.on("connection", (socket) => {
       color: playerColor,
       isSpymaster: false,
     };
-
-    console.log("Sending board to client:", rooms[roomId].board[0][0]);
 
     socket.emit("room-data", {
       roomId,
@@ -90,14 +86,49 @@ io.on("connection", (socket) => {
     });
   });
 
-  // No reset-game
+  socket.on("join-room", (roomId) => {
+    console.log(`Join room request: ${roomId}`);
+    if (rooms[roomId]) {
+      socket.join(roomId);
+      const playerColor = assignPlayerColor(roomId);
+      rooms[roomId].players[socket.id] = {
+        color: playerColor,
+        isSpymaster: false,
+      };
+
+      socket.emit("room-data", {
+        roomId,
+        message: "You joined the room!",
+        board: rooms[roomId].board,
+        playerColor,
+        players: rooms[roomId].players,
+        gameStatus: rooms[roomId].gameStatus,
+        currentTeam: rooms[roomId].currentTeam,
+        spymasters: rooms[roomId].spymasters,
+      });
+
+      socket.to(roomId).emit("room-data", {
+        message: `Client ${socket.id} joined the room!`,
+        board: rooms[roomId].board,
+        players: rooms[roomId].players,
+        gameStatus: rooms[roomId].gameStatus,
+        currentTeam: rooms[roomId].currentTeam,
+        spymasters: rooms[roomId].spymasters,
+      });
+    } else {
+      socket.emit("room-data", { message: "Room does not exist" });
+    }
+  });
+
   socket.on("reset-game", (roomId) => {
     const room = rooms[roomId];
     if (room) {
-      room.board = createInitialBoard();
+      const newBoard = createInitialBoard();
+
+      room.board = newBoard;
       room.gameStatus = "playing";
       room.blackWordRevealed = false;
-      room.currentTeam = "red"; // VERMELHO SEMPRE PRIMEIRO
+      room.currentTeam = "red";
 
       io.to(roomId).emit("room-data", {
         board: room.board,
@@ -110,81 +141,6 @@ io.on("connection", (socket) => {
     }
   });
 
-  // Manipula o ingresso na sala
-  socket.on("join-room", (roomId) => {
-    if (rooms[roomId]) {
-      socket.join(roomId);
-      const playerColor = assignPlayerColor(roomId);
-      rooms[roomId].players[socket.id] = {
-        color: playerColor,
-        isSpymaster: false,
-      };
-
-      // Enviar estado EXATO da sala
-      socket.emit("room-data", {
-        roomId,
-        message: "You joined the room!",
-        board: rooms[roomId].board,
-        playerColor,
-        players: rooms[roomId].players,
-        gameStatus: rooms[roomId].gameStatus,
-        currentTeam: rooms[roomId].currentTeam,
-        spymasters: rooms[roomId].spymasters,
-      });
-
-      socket.to(roomId).emit("room-data", {
-        message: `Client ${socket.id} joined the room!`,
-        board: rooms[roomId].board,
-        players: rooms[roomId].players,
-        gameStatus: rooms[roomId].gameStatus,
-        currentTeam: rooms[roomId].currentTeam,
-        spymasters: rooms[roomId].spymasters,
-      });
-    } else {
-      socket.emit("room-data", { message: "Room does not exist" });
-    }
-  });
-  socket.on("reset-board", (roomId, newBoard) => {
-    // Envia o novo board para todos os jogadores na sala, exceto o que enviou
-    socket.to(roomId).emit("reset-board", newBoard);
-
-    // Se quiser enviar também para quem clicou no reset
-    // io.in(roomId).emit('reset-board', newBoard);
-  });
-  // Manipula o ingresso na sala
-  socket.on("join-room", (roomId) => {
-    console.log(`Join room request: ${roomId}`);
-    if (rooms[roomId]) {
-      socket.join(roomId);
-      console.log(`Client ${socket.id} joined room: ${roomId}`);
-      const playerColor = assignPlayerColor(roomId);
-      rooms[roomId].players[socket.id] = {
-        color: playerColor,
-        isSpymaster: false,
-      };
-      socket.emit("room-data", {
-        roomId,
-        message: "You joined the room!",
-        board: rooms[roomId].board,
-        playerColor,
-        players: rooms[roomId].players,
-        gameStatus: rooms[roomId].gameStatus,
-        currentTeam: rooms[roomId].currentTeam,
-        spymasters: rooms[roomId].spymasters,
-      });
-      socket.to(roomId).emit("room-data", {
-        message: `Client ${socket.id} joined the room!`,
-        board: rooms[roomId].board,
-        players: rooms[roomId].players,
-        gameStatus: rooms[roomId].gameStatus,
-        currentTeam: rooms[roomId].currentTeam,
-        spymasters: rooms[roomId].spymasters,
-      });
-    } else {
-      console.log(`Room ${roomId} does not exist`);
-      socket.emit("room-data", { message: "Room does not exist" });
-    }
-  });
   socket.on("pass-turn", ({ roomId, newTurn }) => {
     const room = rooms[roomId];
     if (room) {
@@ -193,87 +149,14 @@ io.on("connection", (socket) => {
       io.to(roomId).emit("room-data", {
         currentTeam: room.currentTeam,
       });
-
-      io.to(roomId).emit("turn-changed", { newTurn });
     }
   });
-  // Manipula o pedido para ser Spymaster
-  socket.on("set-spymaster", ({ roomId, team }) => {
-    const room = rooms[roomId];
-    if (room) {
-      if (!room.spymasters[team]) {
-        room.spymasters[team] = socket.id;
-        room.players[socket.id].isSpymaster = true;
-        io.to(roomId).emit("room-data", {
-          spymasters: room.spymasters,
-          players: room.players,
-          message: `Player ${socket.id} is now Spymaster for ${team}`,
-        });
-      } else {
-        socket.emit("room-data", {
-          message: "Spymaster already assigned for this team.",
-        });
-      }
-    } else {
-      socket.emit("room-data", { message: "Room does not exist" });
-    }
-  });
-  socket.on("reset-game", (roomId) => {
-    const room = rooms[roomId];
-    if (room) {
-      // Criar novo board
-      const newBoard = createInitialBoard();
 
-      // Atualizar TUDO na sala
-      room.board = newBoard;
-      room.gameStatus = "playing";
-      room.blackWordRevealed = false;
-      room.currentTeam = "blue";
-
-      // Enviar para TODOS na sala o estado COMPLETO
-      io.to(roomId).emit("room-data", {
-        board: room.board,
-        players: room.players,
-        gameStatus: room.gameStatus,
-        blackWordRevealed: room.blackWordRevealed,
-        currentTeam: room.currentTeam,
-        spymasters: room.spymasters,
-      });
-
-      // Também enviar reset-board para garantir
-      io.to(roomId).emit("reset-board", newBoard, "playing", 9, 8);
-    }
-  });
   socket.on("card-clicked", (data) => {
-    console.log("Card clicked event received on server:", data);
     socket.to(data.roomId).emit("card-clicked", data);
   });
 
-  // Manipula a renúncia de Spymaster
-  socket.on("resign-spymaster", (roomId) => {
-    const room = rooms[roomId];
-    if (room) {
-      const player = room.players[socket.id];
-      if (player && player.isSpymaster) {
-        const team = Object.keys(room.spymasters).find(
-          (team) => room.spymasters[team] === socket.id
-        );
-        if (team) {
-          delete room.spymasters[team];
-          player.isSpymaster = false;
-          io.to(roomId).emit("room-data", {
-            spymasters: room.spymasters,
-            players: room.players,
-          });
-        }
-      }
-    }
-  });
-
-  // Manipula atualizações do tabuleiro
   socket.on("update-board", (data) => {
-    console.log("Update board received from socket:", data);
-
     if (rooms[data.roomId]) {
       const room = rooms[data.roomId];
       room.board = data.board;
@@ -281,7 +164,6 @@ io.on("connection", (socket) => {
       room.blackWordRevealed = data.blackWordRevealed;
       room.currentTeam = data.currentTurn;
 
-      // Enviar para TODOS os clientes na sala (incluindo quem enviou)
       io.to(data.roomId).emit("room-data", {
         board: room.board,
         players: room.players,
@@ -290,107 +172,28 @@ io.on("connection", (socket) => {
         currentTeam: room.currentTeam,
         spymasters: room.spymasters,
       });
-
-      // Enviar evento específico de mudança de turno
-      io.to(data.roomId).emit("turn-changed", { newTurn: room.currentTeam });
-
-      console.log(
-        `Board updated and emitted to room: ${data.roomId}, current team: ${room.currentTeam}`
-      );
     }
   });
 
-  socket.on("cell-click", (data) => {
-    const { roomId, row, col } = data;
+  socket.on("game-won", ({ roomId, winnerTeam }) => {
     const room = rooms[roomId];
-    if (!room) return;
-
-    const cell = room.board[row][col];
-    const player = room.players[socket.id];
-
-    if (!player) return;
-    if (!player.isSpymaster && cell.revealed) return;
-
-    if (player.isSpymaster) {
-      room.board = room.board.map((rowArr) =>
-        rowArr.map((cell) => ({ ...cell, revealed: true }))
-      );
-    } else {
-      const newBoard = room.board.map((rowArr, rowIndex) =>
-        rowArr.map((cellItem, colIndex) =>
-          rowIndex === row && colIndex === col
-            ? { ...cellItem, revealed: true }
-            : cellItem
-        )
-      );
-
-      let gameStatus = room.gameStatus;
-      let blackWordRevealed = room.blackWordRevealed;
-      let currentTeam = room.currentTeam;
-
-      if (cell.category === "black") {
-        gameStatus = "lost";
-        blackWordRevealed = true;
-      } else if (cell.category !== currentTeam) {
-        currentTeam = currentTeam === "blue" ? "red" : "blue";
-      }
-
-      room.board = newBoard;
-      room.gameStatus = gameStatus;
-      room.blackWordRevealed = blackWordRevealed;
-      room.currentTeam = currentTeam;
-    }
-
-    io.to(roomId).emit("room-data", {
-      board: room.board,
-      players: room.players,
-      gameStatus: room.gameStatus,
-      blackWordRevealed: room.blackWordRevealed,
-      currentTeam: room.currentTeam,
-    });
-
-    if (room.gameStatus === "lost") {
-      io.to(roomId).emit("game-over", {
-        message: "Game over! Black word revealed.",
-      });
-    }
-  });
-  // socket.on("reset-board", (roomId, newBoard, newStatus) => {
-  //   io.to(roomId).emit("reset-board", newBoard, newStatus);
-  // });
-  // Manipula o pedido para deletar uma sala
-  socket.on("delete-room", (roomId) => {
-    console.log(`Delete room request: ${roomId}`);
-    if (rooms[roomId]) {
-      // Remove a sala
-      delete rooms[roomId];
-      io.emit(
-        "rooms-update",
-        Object.keys(rooms).map((id) => ({ roomId: id }))
-      );
-      console.log(`Room deleted: ${roomId}`);
-      io.to(roomId).emit("room-deleted", {
-        message: `Room ${roomId} has been deleted.`,
-      });
-    } else {
-      socket.emit("room-data", { message: "Room does not exist" });
+    if (room) {
+      room.gameStatus = "finished";
+      io.to(roomId).emit("game-won", { winnerTeam });
     }
   });
 
-  // Limpa a sala quando o cliente desconecta
   socket.on("disconnect", () => {
     console.log(`Client disconnected: ${socket.id}`);
     Object.keys(rooms).forEach((roomId) => {
-      if (io.sockets.adapter.rooms.get(roomId)?.has(socket.id)) {
-        const room = rooms[roomId];
-        delete room.players[socket.id];
+      if (rooms[roomId] && rooms[roomId].players[socket.id]) {
+        delete rooms[roomId].players[socket.id];
         io.to(roomId).emit("room-data", {
           message: `Client ${socket.id} has left the room.`,
-          players: room.players,
+          players: rooms[roomId].players,
         });
 
-        // Se a sala estiver vazia, remova-a
-        if (Object.keys(room.players).length === 0) {
+        if (Object.keys(rooms[roomId].players).length === 0) {
           delete rooms[roomId];
           io.emit(
             "rooms-update",
@@ -402,15 +205,6 @@ io.on("connection", (socket) => {
   });
 });
 
-function shuffleArray(array) {
-  // Cria uma cópia do array para não alterar o original
-  const shuffled = [...array];
-  for (let i = shuffled.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
-  }
-  return shuffled;
-}
 function createInitialBoard() {
   const categories = [
     "red",
@@ -439,41 +233,113 @@ function createInitialBoard() {
     "neutral",
     "black",
   ];
-
   const shuffledCategories = shuffleArray([...categories]);
   const uniqueWords = [...new Set(words)];
   const shuffledWords = shuffleArray([...uniqueWords]).slice(0, 25);
 
-  // Gerar índices fixos para as imagens no servidor
-  const redIndices = shuffleArray(Array.from({ length: 9 }, (_, i) => i));
-  const blueIndices = shuffleArray(Array.from({ length: 8 }, (_, i) => i));
-
-  let redCounter = 0;
-  let blueCounter = 0;
-
   const board = [];
-  for (let row = 0; row < 5; row++) {
-    board[row] = [];
-    for (let col = 0; col < 5; col++) {
-      const index = row * 5 + col;
-      const category = shuffledCategories[index];
+  let redIndex = 0;
+  let blueIndex = 0;
 
-      let imageIndex = 0;
-      if (category === "red") {
-        imageIndex = redIndices[redCounter++];
-      } else if (category === "blue") {
-        imageIndex = blueIndices[blueCounter++];
-      }
+  for (let i = 0; i < 25; i++) {
+    const row = Math.floor(i / 5);
+    const col = i % 5;
 
-      board[row][col] = {
-        word: shuffledWords[index],
-        revealed: false,
-        category: category,
-        imageIndex: imageIndex,
-      };
+    if (!board[row]) board[row] = [];
+
+    const category = shuffledCategories[i];
+    let imageIndex = 0;
+
+    if (category === "red") {
+      imageIndex = redIndex;
+      redIndex = (redIndex + 1) % 9;
+    } else if (category === "blue") {
+      imageIndex = blueIndex;
+      blueIndex = (blueIndex + 1) % 8;
     }
+
+    board[row][col] = {
+      word: shuffledWords[i],
+      revealed: false,
+      category: category,
+      imageIndex: imageIndex,
+    };
   }
 
+  return board;
+}
+
+function shuffleArray(array) {
+  // Cria uma cópia do array para não alterar o original
+  const shuffled = [...array];
+  for (let i = shuffled.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
+  }
+  return shuffled;
+}
+function createInitialBoard() {
+  const board = [];
+  const categories = [
+    "red",
+    "red",
+    "red",
+    "red",
+    "red",
+    "red",
+    "red",
+    "red",
+    "red",
+    "blue",
+    "blue",
+    "blue",
+    "blue",
+    "blue",
+    "blue",
+    "blue",
+    "blue",
+    "neutral",
+    "neutral",
+    "neutral",
+    "neutral",
+    "neutral",
+    "neutral",
+    "neutral",
+    "black",
+  ];
+  const shuffledCategories = shuffleArray([...categories]);
+  const uniqueWords = [...new Set(words)];
+  const shuffledWords = shuffleArray([...uniqueWords]).slice(0, 25);
+
+  let redIndex = 0;
+  let blueIndex = 0;
+
+  for (let i = 0; i < 25; i++) {
+    const row = Math.floor(i / 5);
+    const col = i % 5;
+
+    if (!board[row]) board[row] = [];
+
+    const category = shuffledCategories[i];
+    let imageIndex = 0;
+
+    if (category === "red") {
+      imageIndex = redIndex;
+      redIndex = (redIndex + 1) % 9;
+    } else if (category === "blue") {
+      imageIndex = blueIndex;
+      blueIndex = (blueIndex + 1) % 8;
+    }
+
+    board[row][col] = {
+      word: shuffledWords[i],
+      revealed: false,
+      category: category,
+      imageIndex: imageIndex,
+    };
+  }
+
+  console.log("PRIMEIRO CARD CRIADO:", board[0][0]);
   return board;
 }
 
